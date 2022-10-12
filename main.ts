987d05da7d63a087b7b9f96cf19d988ffb4bf01c
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-12 11:51:06 +0500

    telegraf commands intro

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
 import {Telegraf} from 'telegraf'
 import {config} from 'dotenv'
 import 'colors'
+import {Update} from 'typegram'
 
 config()
 
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`)
 
+bot.use(async function (ctx, next) {
+    console.log(new Date().toString())
+    ctx.state.user = {name: 'John', age: 23}
+    await next()
+})
+
+bot.catch(function (err, ctx) {
+    console.log('ERROR', err)
+})
+
 bot.start(function (ctx, next) {
+    console.log(ctx.state)
     ctx.reply('Start command')
 })
 
@@ -16,6 +28,33 @@ bot.help(function (ctx) {
 
 bot.settings(function (ctx) {
     ctx.reply('Setting command')
+})
+
+bot.command(['stop', 'finish'], function (ctx) {
+    ctx.reply('Stop commmand ✋  ')
+})
+
+bot.mention('botfather', function (ctx) {
+    ctx.reply('Bot father mention 🅱')
+})
+
+bot.phone('+381991231231', function (ctx) {
+    ctx.reply('This is +381991231231!📱')
+})
+
+bot.command('ctx', function (ctx) {
+    console.log(ctx.update.message.from.first_name)
+    ctx.reply(`Hi, ${ctx.update.message.from.first_name} ✋`)
+})
+
+bot.hears('user', function (ctx) {
+    console.log(ctx.state)
+    ctx.reply(JSON.stringify(ctx.state))
+})
+
+bot.on(['message', 'edited_message'], function (ctx) {
+    console.log(ctx.message)
+    console.log(ctx.updateType)
 })
 
 async function launchBot() {
